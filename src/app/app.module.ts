3e51815fcc2873bebeb8a44c730876ab4956953b
Eduardo Valenzuela <evalenzuela@aurasix.com>
2018-10-25 23:45:24 -0500

    Add base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
+import { ProfileFormComponent } from './profile-form/profile-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileDetailComponent,
+    ProfileFormComponent
   ],
   imports: [
     BrowserModule
